3856770cfe26ed0ee5b80ac04651cfe34481edc3
Jack Tiong <tiongyingkiat@gmail.com>
2023-11-01 23:44:20 +0800

    Updated constants.js

---
+++ constants/constants.js
@@ -0,0 +1,24 @@
+const onboarding_screens = [
+    {
+        id: 1,
+        image: require('../assets/images/splash_1.png'),
+        title: "Search your trip easily",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue."
+    },
+    {
+        id: 2,
+        image: require('../assets/images/splash_2.png'),
+        title: "Explore destinations and promotions",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue."
+    },
+    {
+        id: 3,
+        image: require('../assets/images/splash_3.png'),
+        title: "Manage your trips",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus varius nulla, quis dictum augue."
+    }
+]
+
+export default {
+    onboarding_screens
+}
--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,8 @@
+import constants from "./constants";
 import theme, { COLORS, SIZES, FONTS } from "./theme";
 
 export {
+    constants,
     theme,
     COLORS,
     SIZES,
